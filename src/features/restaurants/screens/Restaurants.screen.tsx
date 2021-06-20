8a762f864c4827451359b9971bfd56a65d6c153a
Shubham Vishwakarma <Shubham Vishwakarma>
2021-06-20 17:40:15 +0530

    Flat List Added to Restaurants Screen!

--- src/features/restaurants/screens/Restaurants.screen.tsx
+++ src/features/restaurants/screens/Restaurants.screen.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, SafeAreaView, StatusBar } from "react-native";
+import { View, SafeAreaView, StatusBar, FlatList } from "react-native";
 import styled from "styled-components/native";
 import { SearchBar } from "../../../components/SearchBar";
+import { Spacer } from "../../../components/spacer/Spacer";
 import { RestaurantInfoCard } from "../components/RestaurantInfoCard.component";
 
 // to keep space for current status bar
@@ -16,10 +17,12 @@ const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const ListContainer = styled(View)`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const ListContainer = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+    paddingTop: 0,
+  },
+})``;
 /* background-color: ${(props) => props.theme.colors.bg.secondary}; */
 
 export const RestaurantsScreen = () => {
@@ -28,9 +31,82 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <SearchBar />
       </SearchContainer>
-      <ListContainer>
-        <RestaurantInfoCard />
-      </ListContainer>
+      <ListContainer
+        data={[
+          {
+            name: 1,
+          },
+          {
+            name: 2,
+          },
+          {
+            name: 3,
+          },
+          {
+            name: 4,
+          },
+          {
+            name: 5,
+          },
+          {
+            name: 6,
+          },
+          {
+            name: 7,
+          },
+          {
+            name: 8,
+          },
+          {
+            name: 9,
+          },
+          {
+            name: 10,
+          },
+          {
+            name: 11,
+          },
+          {
+            name: 12,
+          },
+          {
+            name: 13,
+          },
+          {
+            name: 14,
+          },
+          {
+            name: 15,
+          },
+          {
+            name: 16,
+          },
+          {
+            name: 17,
+          },
+          {
+            name: 18,
+          },
+          {
+            name: 19,
+          },
+          {
+            name: 20,
+          },
+          {
+            name: 21,
+          },
+          {
+            name: 22,
+          },
+        ]}
+        renderItem={() => (
+          <Spacer position={"bottom"} size={"lg"}>
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item: any) => "key-" + item.name}
+      />
     </SafeArea>
   );
 };
