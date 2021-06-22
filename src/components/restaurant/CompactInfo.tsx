9929539857e83da65efaf85123658698ddf2f311
Shubham Vishwakarma <Shubham Vishwakarma>
2021-06-22 23:51:38 +0530

    Callout Handling Added - 2

---
+++ src/components/restaurant/CompactInfo.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components/native";
+import WebView from "react-native-webview";
+import { Platform } from "react-native";
+
+import { Text } from "../typography/Text";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebview = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+
+export const CompactRestaurantInfo = ({ restaurant }: { restaurant: any }) => {
+  // Due to issue in which in case of maps images are not rendered by Android
+  // Hence we use react-native-webview for the same which is rendered properly in android
+  const Image = isAndroid ? CompactWebview : CompactImage;
+
+  return (
+    <Item>
+      <Image source={{ uri: restaurant.photos[0] }} />
+      <Text variant="caption" numberOfLines={3}>
+        {restaurant.name}
+      </Text>
+    </Item>
+  );
+};
